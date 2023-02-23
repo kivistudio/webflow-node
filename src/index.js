aaf6576e5f0826929e5897f27f7825de9866100c
Ela <elzbieta.siwy@gmail.com>
2023-02-23 00:22:44 +0000

    Styles

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import _ from "lodash";
+import "./style.css";
 
 function component() {
   const element = document.createElement("div");
 
   // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.innerHTML = _.join(["Hello", "colours"], " ");
+  element.classList.add("kivi-red");
 
   return element;
 }
