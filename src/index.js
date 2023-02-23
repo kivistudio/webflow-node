7224698becbfd2d7821d1f610a1dc5c95f8e2779
Ela <elzbieta.siwy@gmail.com>
2023-02-23 00:39:05 +0000

    Images

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import _ from "lodash";
+
 import "./style.css";
+import Icon from "./logo.svg";
 
 function component() {
   const element = document.createElement("div");
@@ -7,6 +9,11 @@ function component() {
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(["Hello", "colours"], " ");
   element.classList.add("kivi-red");
+
+  const myIcon = new Image();
+  myIcon.src = Icon;
+  myIcon.classList.add("logo");
+  element.appendChild(myIcon);
 
   return element;
 }
